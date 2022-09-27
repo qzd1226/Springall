c41db1f03fa752dafb571b9efb41808511b5b2ce
qzd1226 <690160142@qq.com>
2022-09-27 17:25:43 -0500

    加入Spring创建对象的方法

--- spring_001_demo/src/test/java/MyTest.java
+++ spring_001_demo/src/test/java/MyTest.java
@@ -1,9 +1,21 @@
 import org.example.pojo.Student;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyTest {
     @Test
     public void test(){
         Student stu = new Student();
+        System.out.println(stu);
+    }
+    @Test
+    public void testSprinng(){
+        // create object by spring
+
+        //如果想从spring 容器取中对象，则需要先创建容器对象并启动才可以取对象
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student stu = (Student) ac.getBean("stu");
         System.out.println(stu);
     }
 }
