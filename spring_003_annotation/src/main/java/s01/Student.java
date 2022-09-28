f43d97e814ea0436229c07c2760950677c65973d
qzd1226 <690160142@qq.com>
2022-09-28 01:19:35 -0500

    @Value 设置初值

--- spring_003_annotation/src/main/java/s01/Student.java
+++ spring_003_annotation/src/main/java/s01/Student.java
@@ -1,11 +1,14 @@
 package s01;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component // 交给spring 去创建对象,就是在容器启动时创建
 public class Student {
+    @Value("张三")
     private String name;
+    @Value("18")
     private  int age;
 
     public Student() {
