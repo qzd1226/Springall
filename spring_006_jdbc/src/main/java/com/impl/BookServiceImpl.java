663bc5d770969316c0e5e98c0104e525975463d5
qzd1226 <690160142@qq.com>
2022-09-29 21:27:57 -0500

    声明式事务属性： 只读，超时，回滚

--- spring_006_jdbc/src/main/java/com/impl/BookServiceImpl.java
+++ spring_006_jdbc/src/main/java/com/impl/BookServiceImpl.java
@@ -12,7 +12,11 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookDao bookDao;
     @Override
-    @Transactional // 加入事务管理
+    // readOnly: 只读 用于查询时设置为true
+    // timeout: 限制时间，超过时间异常跳出，强制回滚
+    // rollBackFor : 因为什么而回滚
+    // noRollBackFor: 不因为什么而回滚
+    @Transactional(readOnly = false, timeout = 3) // 加入事务管理
     public void buyBook(Integer userId, Integer bookId) {
         //查询图书馆的价格
         Integer price = bookDao.getPriceByBookId(bookId);
